de200cd42f45ee073ea020773ba25a0e48a021b2
Kristian <krbrudeli@gmail.com>
2019-09-27 14:19:54 +0200

    Assgn. 3 ex. 4 done

--- 03/oppg4.jl
+++ 03/oppg4.jl
@@ -2,12 +2,8 @@ x = [1.0 2.0; 2.0 3.0; 3.0 2.0; 4.0 5.0; 6.0 6.0; 7.0 1.0]
 
 y = [7.0 1.0; 1.0 2.0; 3.0 2.0; 2.0 3.0; 4.0 5.0; 6.0 6.0]
 
-function rowisinarray(arr, row)
-    # num_cols = size(arr)[1]
-    num_cols = 2
-
-    return any([row' == arr[i, :] for i in 1:num_cols])
-end
+x2 = [5.0 5.0; 5.0 10.0; 5.0 9.0; 5.0 1.0; 5.0 2.0]
+y2 = [5.0 1.0; 5.0 2.0; 5.0 5.0; 5.0 9.0; 5.0 10.0]
 
 function splitintwo(x, y)
     len = size(x)[1]
@@ -19,15 +15,23 @@ function splitintwo(x, y)
     left_len = size(x_left)[1]
     right_len = size(x_right)[1]
 
-    println(len)
-    println(x_left)
-    println(x_right)
-
     y_left = similar(x_left)
     y_right = similar(x_right)
 
     left_it = 1
     right_it = 1
+
+    y_split = maximum(x_left[2,:])
+
+    y_coords_in_left_at_median = Set()
+    #  = [row[2] for row in eachrow(x_left) if row[1] == x_left[end, 1]] # if eachrow is defined
+    for r in 1:left_len
+        if x_left[r, 1] == x_left[end, 1]
+            push!(y_coords_in_left_at_median, x_left[r, 2])
+        end 
+    end
+
+    # If multiple rows have the same x-coord as the median, it should still be placed in the right list
     
     for k = 1:len
         # Place in right if x > split
@@ -36,7 +40,10 @@ function splitintwo(x, y)
         if y[k, 1] > x_left[end, 1]
             y_right[right_it, :] = y[k, :]
             right_it += 1
-        elseif left_it <= left_len
+        elseif y[k, 1] < x_right[1, 1]
+            y_left[left_it, :] = y[k, :]
+            left_it += 1
+        elseif y[k, 2] in y_coords_in_left_at_median && left_it <= left_len
             y_left[left_it, :] = y[k, :]
             left_it += 1
         else
@@ -48,3 +55,4 @@ function splitintwo(x, y)
 end
 
 println(splitintwo(x, y))
+println(splitintwo(x2, y2))
