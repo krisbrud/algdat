b71d37ac469dad8cf7ebfab281e012d2066c501f
Kristian <krbrudeli@gmail.com>
2019-10-17 22:36:56 +0200

    6-1 done

--- 06/ov6-1.jl
+++ 06/ov6-1.jl
@@ -1,29 +1,30 @@
 function lislength(s)
-    mls = zeros(Int, size(s))
-    mls[1] = 1
-
-    longest = 0
+    mls = ones(Int, size(s))
 
     for i = 2:length(s)
-        # Din kode her
-        if s[i] > s[i-1]
-            longest += 1
-            mls[i] = max(longest, mls[i-1])
-        else
-            longest = 1
+        for j = 1:i-1
+            if s[i] > s[j]
+                # Irrelevant which subsequence of the same length we pick,
+                # as every subseq. containing this number must have the next 
+                # number must also contain this one
+                mls[i] = max(mls[i], mls[j] + 1)
+            end
         end
     end
     return maximum(mls) # Returnerer det største tallet i listen
 end
 
 
-function test_sequence(s, expected)
-    println("Maximum increasing subsequence of $s: $(lislength(s)), expected $expected")
-end
+printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
 
-test_sequence([5,3,3,6,7], 3)
-test_sequence([5,2,3,6,7], 4)
-test_sequence([5,6,3,6,2], 2)
-test_sequence([5,4,3,2,1], 1)
+using Test
+@testset "Tester" begin
+	@test lislength([5,3,3,6,7]) == 3
+	@test lislength([2,2,2,2]) == 1
+	@test lislength([100,50,25,10]) == 1
+	@test lislength([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]) == 6
+	@test lislength([682, 52, 230, 441, 1000, 22, 678, 695, 0, 681]) == 5
+	@test lislength([441, 1000, 22, 678, 695, 0, 681, 956, 48, 587, 604, 857, 689, 346, 425, 513, 476, 917, 114, 43, 327, 172, 162, 76, 91, 869, 549, 883, 679, 812, 747, 862, 228, 368, 774, 838, 107, 738, 717, 25, 937, 927, 145, 44, 634, 557, 850, 965]) == 12
+end
 
 
